5e5dc463f834e7bbf66af81eee4717bdf8aa2cc8
Steven Bull <steven@thebulls.us>
2013-10-15 16:00:35 -0700

    Corrected comment typo.

--- lib/banana_peels/mailer_helper.rb
+++ lib/banana_peels/mailer_helper.rb
@@ -1,6 +1,6 @@
 module BananaPeels::MailerHelper
 
-  # Do "include BananaPeels::MailerHelper in an ActionMailer.
+  # Do "include BananaPeels::MailerHelper" in an ActionMailer.
 
   def banana_mail(campaign_id, mail_options, merge_tags, api_key)
     BananaPeels.mail(self, campaign_id, mail_options, merge_tags, api_key)
