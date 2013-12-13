30897fd37372cf57d544710c76d105eb838d20bb
Steven Bull <steven@thebulls.us>
2013-12-12 16:22:56 -0800

    Enabled specification of either "from_name" or "from_email" instead of just "from".

--- lib/banana_peels/api.rb
+++ lib/banana_peels/api.rb
@@ -27,9 +27,16 @@ module BananaPeels
     end
 
     # Use this instead of #mail() in ActionMailer.
+    # Also allows :from_name, :from_email to be specified independently
+    # as mail_options as opposed to just :from.
     def mail(mailer, campaign_id, mail_options, merge_tags, api_key)
       c = campaign(campaign_id, merge_tags, api_key)
-      mailer.mail(c.default_mail_options.merge(mail_options)) do |format|
+      headers = c.default_mail_options.merge(mail_options)
+      # Adjust from address.
+      if !mail_options[:from] && (mail_options[:from_name] || mail_options[:from_email])
+        headers[:from] = email_with_name(mail_options[:from_name] || c.from_name, mail_options[:from_email] || c.from_email)
+      end
+      mailer.mail(headers) do |format|
         format.text { mailer.render text: c.text_content }
         format.html(content_transfer_encoding: 'quoted-printable') do
           mailer.render text: [c.html_content].pack('M')
